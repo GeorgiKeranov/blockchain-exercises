5aa25f26e62fa4dd7949e9d24a2e92187927737a
"Georgi Keranov" <"georgy.keranov@gmail.com">
2022-06-27 16:15:33 +0300

    Add a new functionality to buy a product

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -12,23 +12,67 @@ contract Store is Ownable {
     }
 
     Product[] public products;
-    mapping(string => bool) public availableProducts;
+    mapping(string => bool) public availableProductsNames;
+
+    // Product id => (client address => product quantity)
+    mapping(uint => mapping(address => uint)) public productsPurchases;
+
+    event ProductAdded(string, uint, uint);
+    event ProductUpdated(uint, uint);
+    event ProductBought(address, uint, uint);
 
     function addNewProduct(string calldata name, uint quantity, uint price) public onlyOwner {
-        require(name.length && price > 0)
-        require(!availableProducts[name], "The product is already in the store!");
+        require(bytes(name).length != 0 && price > 0);
+        require(!availableProductsNames[name], "The product is already in the store!");
         
-        availableProducts[name] = true;
-        products.push(Product(products.length, name, quantity, price));
+        uint productId = products.length;
+
+        availableProductsNames[name] = true;
+        products.push(Product(productId, name, quantity, price));
+
+        emit ProductAdded(name, quantity, price);
     }
 
-    function updateProductQuantity(uint id, uint quantity) public onlyOwner {
+    modifier onlyExistingProduct(uint id) {
         require(id < products.length, "The product is not existing!");
+        _;
+    }
 
+    function updateProductQuantity(uint id, uint quantity) public onlyOwner onlyExistingProduct(id) {
         products[id].quantity = quantity;
+        
+        emit ProductUpdated(id, quantity);
     }
 
     function getProducts() public view returns(Product[] memory) {
         return products;
+    }
+
+    function buyProduct(uint id, uint quantity) payable public onlyExistingProduct(id) {
+        require(quantity > 0, "Invalid quantity!");
+
+        address clientAddress = msg.sender;
+        
+        // Check if the user has already bought the product
+        require(productsPurchases[id][clientAddress] == 0, "You have already bought this product!");
+
+        Product storage currentProduct = products[id];
+
+        // Check if we have enough quantity from the product
+        require(currentProduct.quantity >= quantity, "The product quantity is less than you wanted!");
+
+        uint256 totalPrice = uint256(products[id].price * quantity);
+        require(msg.value == totalPrice, "Please send the exact price of the product multiplied by the quantity you want!");
+
+        // Transfer the ether to the owner of the store
+        payable(owner).transfer(totalPrice);
+
+        // Add the new purchase to the state
+        productsPurchases[id][clientAddress] = quantity;
+
+        // Remove the bought quantity from the product
+        currentProduct.quantity -= quantity;
+
+        emit ProductBought(clientAddress, id, quantity);
     }
 }
