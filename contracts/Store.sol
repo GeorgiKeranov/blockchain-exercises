485b1cc19287c9fbf31f576647c6a80f168911fd
"Georgi Keranov" <"georgy.keranov@gmail.com">
2022-06-27 10:49:50 +0300

    Add Store contract and ability to add new product to the store

---
+++ contracts/Store.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.7;
+
+import "./Ownable.sol";
+
+contract Store is Ownable {
+    struct Product {
+        string name;
+        uint quantity;
+        uint price;
+    }
+
+    Product[] public products;
+    mapping(string => bool) public availableProducts;
+
+    function addNewProduct(string calldata name, uint quantity, uint price) public onlyOwner {
+        require(!availableProducts[name], "The product is already in the store!");
+        
+        availableProducts[name] = true;
+        products.push(Product(name, quantity, price));
+    }
+}
