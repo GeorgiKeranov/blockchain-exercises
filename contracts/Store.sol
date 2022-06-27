d857f3b851b9bd073d9aa0e889df6ec561ac973f
"Georgi Keranov" <"georgy.keranov@gmail.com">
2022-06-27 11:26:46 +0300

    Add functionality to get products

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -5,6 +5,7 @@ import "./Ownable.sol";
 
 contract Store is Ownable {
     struct Product {
+        uint id;
         string name;
         uint quantity;
         uint price;
@@ -14,15 +15,20 @@ contract Store is Ownable {
     mapping(string => bool) public availableProducts;
 
     function addNewProduct(string calldata name, uint quantity, uint price) public onlyOwner {
+        require(name.length && price > 0)
         require(!availableProducts[name], "The product is already in the store!");
         
         availableProducts[name] = true;
-        products.push(Product(name, quantity, price));
+        products.push(Product(products.length, name, quantity, price));
     }
 
-    function updateProductQuantity(uint index, uint quantity) public onlyOwner {
-        require(index < products.length, "The product is not existing!");
+    function updateProductQuantity(uint id, uint quantity) public onlyOwner {
+        require(id < products.length, "The product is not existing!");
 
-        products[index].quantity = quantity;
+        products[id].quantity = quantity;
+    }
+
+    function getProducts() public view returns(Product[] memory) {
+        return products;
     }
 }
